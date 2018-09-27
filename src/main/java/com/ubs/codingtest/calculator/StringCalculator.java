e50633c5b40862b69b33652d7e01031497413672
SHANK-PC\Shashank <shank538@gmail.com>
2018-09-27 08:44:37 +0800

    added loging

--- src/main/java/com/ubs/codingtest/calculator/StringCalculator.java
+++ src/main/java/com/ubs/codingtest/calculator/StringCalculator.java
@@ -6,6 +6,8 @@ import com.ubs.codingtest.strategy.ParsingStrategy;
 import com.ubs.codingtest.util.Constants;
 import com.ubs.codingtest.util.DelimiterUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -21,14 +23,20 @@ public class StringCalculator {
         this.parsingStrategy = parsingStrategy;
     }
 
+    Logger logger = LoggerFactory.getLogger(StringCalculator.class);
+
     public int add(String numbers){
 
         int total = 0;
         if(!StringUtils.isEmpty(numbers)) {
 
+            logger.info("Processing numbers string : {}" + numbers);
             Map<String,String> delimiterMap = parsingStrategy.getDelimiterAndNumbers(numbers);
             String delimiter = delimiterMap.get(Constants.DELIMITER);
             numbers = delimiterMap.get(Constants.NUMBERS);
+
+            logger.info("Delimiter for the input string : {}", delimiter);
+            logger.info("Numbers for the input string : {}", numbers);
 
             List<Integer> negativeValues = new ArrayList();
 
@@ -47,14 +55,17 @@ public class StringCalculator {
                         .collect(Collectors.summingInt(Integer::intValue));
 
             }catch(NumberFormatException  nfe){
+                logger.error("Invalid delimiter supplied : {}",  numbers);
                 throw new InvalidDelimiter("Invalid delimiter supplied : "+ numbers);
             }
 
             if(negativeValues.size() > 0){
+                logger.info("negatives not allowed : {}",  negativeValues);
                 throw new InvalidNumberException("negatives not allowed : "+ negativeValues);
             }
         }
 
+        logger.info("Total for input string : {}", total);
         return total;
     }
 
